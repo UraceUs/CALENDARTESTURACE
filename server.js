e4e19c221c4a27220ab98d452a9f9672a7473323
Eduardo <eduardoresende.work@gmail.com>
2026-04-23 18:13:20 -0400

    format: padroniza datas para MM--DD-YYYY em todo o fluxo

--- server.js
+++ server.js
@@ -728,7 +728,7 @@ function formatDateBr(dateString) {
   }
 
   const [year, month, day] = dateString.split('-');
-  return `${day}/${month}/${year}`;
+  return `${month}--${day}-${year}`;
 }
 
 function periodLabel(periodo) {
